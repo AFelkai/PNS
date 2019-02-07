167f36087adf7f449d8ae228ff0503473116bc79
Ryan <ryan@Ryans-MacBook-Pro-15.local>
2019-02-07 10:26:04 -0800

    adding angular

---
+++ frontend/src/app/components/splash-image/splash-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-splash-image',
+  templateUrl: './splash-image.component.html',
+  styleUrls: ['./splash-image.component.css']
+})
+export class SplashImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
