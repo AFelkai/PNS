5c51cea7f6d8c95f103420c33b41b7d9cb0debb7
Roberto E. Gonzalez <roberto.tico.gonzalez@gmail.com>
2019-04-16 12:10:17 -0700

    Some Angular changes...

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Router, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SplashImageComponent } from './components/splash-image/splash-image.component';
@@ -9,7 +11,24 @@ import { HighlightsComponent } from './components/highlights/highlights.componen
 import { LoginComponent } from './components/login/login.component';
 import { RequestPageComponent } from './components/request-page/request-page.component';
 
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+
+
+]
 @NgModule({
+  
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
+    BrowserModule,
+    //AppRoutingModule
+  ],
+  
   declarations: [
     AppComponent,
     NavbarComponent,
@@ -18,11 +37,8 @@ import { RequestPageComponent } from './components/request-page/request-page.com
     LoginComponent,
     RequestPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
